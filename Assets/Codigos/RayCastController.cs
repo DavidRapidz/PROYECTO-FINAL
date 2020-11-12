c6fe4990ca3066ac5a7e318d7ee90a6293381cf9
DavidRapidz <david_san.sanchez@uao.edu.co>
2020-11-12 11:56:16 -0500

    Initial commit

---
+++ Assets/Codigos/RayCastController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+
+public class RayCastController : MonoBehaviour
+{
+
+    public float range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, range))
+            {
+                Debug.Log(hit.collider.name);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * range);
+    }
+}
